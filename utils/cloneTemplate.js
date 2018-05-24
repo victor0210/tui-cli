ee8eabb2a1f099f513f0684c3b9200fbfcf2f996
github_zhouzhixiang <494755899@qq.com>
2018-05-24 11:54:42 +0800

    aa

--- utils/cloneTemplate.js
+++ utils/cloneTemplate.js
@@ -6,7 +6,7 @@ var templatePath = 'https://github.com/Bennnis/tui-temp.git'
 var cloneTemplate = function (dirname) {
   return new Promise(function (resolve, reject) {
     console.log(color.yellow('template downloading...'))
-    if (shx.exec('git clone ' + templatePath + ' ' + dirname, {silent: true}).code === 0) { //success
+    if (shx.exec('git cloness ' + templatePath + ' ' + dirname, {silent: true}).code === 0) { //success
       resolve()
     } else {
       reject(new Error('error in clone template, please check network'))
